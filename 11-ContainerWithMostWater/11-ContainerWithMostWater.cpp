9f495fa15820cb88ee27f54301a3662800bc6c2f
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2025-08-01 23:40:56 +0530

    Create 11-ContainerWithMostWater

---
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
@@ -0,0 +1,20 @@
+// Last updated: 8/1/2025, 11:41:00 PM
+class Solution {
+ public:
+  int maxArea(vector<int>& height) {
+    int ans = 0;
+    int l = 0;
+    int r = height.size() - 1;
+
+    while (l < r) {
+      const int minHeight = min(height[l], height[r]);
+      ans = max(ans, minHeight * (r - l));
+      if (height[l] < height[r])
+        ++l;
+      else
+        --r;
+    }
+
+    return ans;
+  }
+};
