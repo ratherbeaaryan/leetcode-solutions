453d993114a7ca120adc8ede7766f89235afbeb4
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2025-08-01 23:39:15 +0530

    Create 3291-FindIfArrayCanBeSorted

---
+++ 3291-FindIfArrayCanBeSorted/3291-FindIfArrayCanBeSorted.cpp
@@ -0,0 +1,33 @@
+// Last updated: 8/1/2025, 11:39:20 PM
+class Solution {
+public:
+    bool canSortArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<pair<int, int>> arr(n);
+        
+        
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            int count = 0;
+            while (num > 0) {
+                count += (num & 1);
+                num = num/2;
+            }
+            arr[i] = {nums[i], count};
+        }
+        
+        
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j].first > arr[j + 1].first) {
+                    
+                    if (arr[j].second != arr[j + 1].second) {
+                        return false;
+                    }
+                    swap(arr[j], arr[j + 1]);
+                }
+            }
+        }
+        return true;
+    }
+};
