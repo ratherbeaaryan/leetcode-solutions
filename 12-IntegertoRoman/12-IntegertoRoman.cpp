b822ddfc9232161f3dedce92496dbee8f1e60d4b
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2025-08-08 09:09:29 +0530

    Create 12-IntegertoRoman

---
+++ 12-IntegertoRoman/12-IntegertoRoman.cpp
@@ -0,0 +1,15 @@
+// Last updated: 8/8/2025, 9:09:32 AM
+class Solution {
+public:
+    string intToRoman(int num) {
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string hundreds[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string thsd[] = {"","M","MM","MMM"};
+
+        return thsd[num/1000] + hundreds[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+
+        
+        
+    }
+};
