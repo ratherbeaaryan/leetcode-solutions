eb53abe44bda6d553387b3184b8832415a460331
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2025-08-22 00:44:27 +0530

    File updated at 8/22/2025, 12:44:28 AM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.cpp
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.cpp
@@ -1,40 +1,46 @@
-// Last updated: 8/22/2025, 12:25:14 AM
+// Last updated: 8/22/2025, 12:44:28 AM
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
-        if (n <= 1) {
+        if(s.size()<=1){
             return s;
         }
-
         int start = 0;
+        int n = s.size();
         int maxLength = 1;
 
-        for (int i = 0; i < n; i++) {
-            // Expand for odd length palindromes (e.g., "aba")
-            int left = i, right = i;
-            while (left >= 0 && right < n && s[left] == s[right]) {
-                int currentLength = right - left + 1;
-                if (currentLength > maxLength) {
+        for(int i=0;i<s.size();i++){
+            //odd
+            int left = i,right = i;
+            while(left>=0 && right<n && s[left]==s[right]){
+                int currentLength = right - left +1;
+                if(currentLength > maxLength){
                     maxLength = currentLength;
                     start = left;
                 }
+                
                 left--;
                 right++;
-            }
+                
 
-            // Expand for even length palindromes (e.g., "abba")
-            left = i, right = i + 1;
-            while (left >= 0 && right < n && s[left] == s[right]) {
-                int currentLength = right - left + 1;
-                if (currentLength > maxLength) {
+            }
+            //even
+            left = i,right = i+1;
+            while(left>=0 && right<n && s[left]==s[right]){
+                
+                int currentLength = right - left +1;
+                if(currentLength > maxLength){
                     maxLength = currentLength;
                     start = left;
                 }
+                
                 left--;
                 right++;
+
             }
+            
+
         }
-        return s.substr(start, maxLength);
+        return s.substr(start,maxLength);
     }
 };
