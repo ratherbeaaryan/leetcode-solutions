98cb9414168eceb410ad54cf588ecb51df941eb7
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2025-08-22 23:51:17 +0530

    Create 1605-MinimumNumberOfDaysToMakeMBouquets

---
+++ 1605-MinimumNumberOfDaysToMakeMBouquets/1605-MinimumNumberOfDaysToMakeMBouquets.cpp
@@ -0,0 +1,42 @@
+// Last updated: 8/22/2025, 11:51:20 PM
+class Solution {
+public:
+    int minDays(vector<int>& nums, int m, int k) {
+        if (nums.size() < (long long)m * k) {
+            return -1;
+        }
+
+        int low = *min_element(nums.begin(), nums.end());
+        int high = *max_element(nums.begin(), nums.end());
+        
+        while (low <= high) {
+            int mid =low +(high-low)/ 2;
+            if (findbloomday(nums, mid, k) < m) {
+                low = mid + 1;
+
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+private:
+    int findbloomday(vector<int>& nums, int day, int flower) {
+        int buq = 0;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] <= day) {
+                count++;
+                if (count == flower) {
+                    buq++;
+                    count = 0;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        return buq;
+    }
+
+};
