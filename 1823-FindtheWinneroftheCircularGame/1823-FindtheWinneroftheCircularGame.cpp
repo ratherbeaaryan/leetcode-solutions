efcd2d49b7636289736d4e8716d79a839d354e3b
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2025-08-29 10:08:39 +0530

    File updated at 8/29/2025, 10:08:40 AM

--- 1823-FindtheWinneroftheCircularGame/1823-FindtheWinneroftheCircularGame.cpp
+++ 1823-FindtheWinneroftheCircularGame/1823-FindtheWinneroftheCircularGame.cpp
@@ -1,35 +1,15 @@
-// Last updated: 8/29/2025, 8:33:14 AM
+// Last updated: 8/29/2025, 10:08:40 AM
 class Solution {
 public:
-    int winner(vector<bool>& person, int n, int index,int personleft,int k){
-        if(personleft == 1){
-            for(int i=0;i<n;i++){
-                if(person[i] == 0){
-                    return i+1;
-                }
-            }
+    int winner(int n,int k){
+        if(n==1){
+            return 0;
         }
-        int kill = (k-1)%personleft;
-        while(kill--){
-            index=(index+1)%n;
-            while(person[index] == 1){
-            index = (index+1)%n;
-        }
-
-        }
-        person[index]=1;
-        while(person[index] == 1){
-            index = (index+1)%n;
-            }
-        return winner(person,n,index,personleft-1,k);
-
+        return (winner(n-1,k)+k)%n;
     }
-
     int findTheWinner(int n, int k) {
-        vector<bool> person(n,0);
         
-        return winner(person,n,0,n,k);
-        
+        return winner(n,k)+1;
         
     }
 };
