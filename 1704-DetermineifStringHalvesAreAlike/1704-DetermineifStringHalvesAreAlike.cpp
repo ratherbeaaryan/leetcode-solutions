1cdac036c7fdd9e91a4f355fdbdaf15dc6974add
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2025-09-07 09:36:05 +0530

    File updated at 9/7/2025, 9:36:09 AM

--- 1704-DetermineifStringHalvesAreAlike/1704-DetermineifStringHalvesAreAlike.cpp
+++ 1704-DetermineifStringHalvesAreAlike/1704-DetermineifStringHalvesAreAlike.cpp
@@ -1,44 +1,19 @@
-// Last updated: 9/7/2025, 9:19:16 AM
+// Last updated: 9/7/2025, 9:36:09 AM
 class Solution {
 public:
-    bool closeStrings(string word1, string word2) {
-        int n = word1.length();
-        int m = word2.length();
-        vector<int> freq1(26);
-        vector<int> freq2(26);
-        if(n!=m){
-            return false;
+    string orderlyQueue(string s, int k) {
+        if(k>1){
+            sort(begin(s),end(s));
+            return s;
         }
-        for(int i=0;i<n;i++){
-            char ch = word1[i];
-            int index = ch-'a';
-            freq1[index]++;
-        }
-        for(int i=0;i<m;i++){
-            char ch = word2[i];
-            int index2 = ch-'a';
-            freq2[index2]++;
-        }
-        for(int i=0;i<26;i++){
-            if(freq1[i]!=0 && freq2[i]!=0){
-                continue;
-            }
-             if(freq1[i]==0 && freq2[i]==0){
-                continue;
-            }
-            
-                return false;
+        string result = s;
+        int n = s.size();
+        for(int l=1;l<=n-1;l++){
+            string temp = s.substr(l)+s.substr(0,l);
+            result = min(result,temp);
 
-        
-            
         }
-        sort(begin(freq1),end(freq1));
-        sort(begin(freq2),end(freq2));
-
-        if(freq1==freq2){
-            return true;
-        }
-        return false;
+        return result;
         
     }
 };
