672d149295fff5b77c3bc931c2e299c49116b2a7
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2026-01-10 23:57:47 +0530

    File updated at 1/10/2026, 11:57:47 PM

--- 29-DivideTwoIntegers/29-DivideTwoIntegers.cpp
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.cpp
@@ -1,12 +1,12 @@
-// Last updated: 8/1/2025, 11:40:54 PM
-class Solution {
-public:
-    int divide(int dividend, int divisor) {
-       
-        if (dividend == INT_MIN && divisor == -1) {
-            return INT_MAX;
-        }
-
-        return dividend / divisor;
-    }
-};
+// Last updated: 1/10/2026, 11:57:47 PM
+1class Solution {
+2public:
+3    int divide(int dividend, int divisor) {
+4       
+5        if (dividend == INT_MIN && divisor == -1) {
+6            return INT_MAX;
+7        }
+8
+9        return dividend / divisor;
+10    }
+11};
