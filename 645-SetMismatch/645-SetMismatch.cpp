55d5d8fb52121b9e1f5fa013e10119c6c35b08cf
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2026-01-14 23:09:24 +0530

    File updated at 1/14/2026, 11:09:25 PM

--- 645-SetMismatch/645-SetMismatch.cpp
+++ 645-SetMismatch/645-SetMismatch.cpp
@@ -1,29 +1,27 @@
-// Last updated: 1/1/2026, 2:59:26 AM
+// Last updated: 1/14/2026, 11:09:25 PM
 1class Solution {
 2public:
-3    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-4        // Step 1: Frequency array (0 to 100)
-5        vector<int> freq(101, 0);
-6        
-7        for (int num : nums) {
-8            freq[num]++;
-9        }
-10        
-11        // Step 2: Prefix sum
-12        for (int i = 1; i <= 100; i++) {
-13            freq[i] += freq[i - 1];
-14        }
-15        
-16        // Step 3: Result array
-17        vector<int> result;
-18        for (int num : nums) {
-19            if (num == 0)
-20                result.push_back(0);
-21            else
-22                result.push_back(freq[num - 1]);
-23        }
+3    vector<int> findDisappearedNumbers(vector<int>& nums) {
+4        int n = nums.size();
+5        vector<int> result;
+6
+7        for(int i=0;i<n;i++){
+8            int idx = abs(nums[i]) - 1;
+9            if(nums[idx]>0){
+10                nums[idx] = -nums[idx];
+11            }
+12        }
+13        for(int i=0;i<n;i++){
+14            if(nums[i]>0){
+15                result.push_back(i+1);
+16
+17            }
+18        }
+19        for(int i=0;i<result.size();i++){
+20            
+21        }
+22
+23        return result;
 24        
-25        return result;
-26    }
-27};
-28
+25    }
+26};
