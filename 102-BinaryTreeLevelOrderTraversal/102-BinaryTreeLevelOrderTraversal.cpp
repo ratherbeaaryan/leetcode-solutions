650bb33599965ecd0174a2ea1ae1a46323034154
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2026-01-20 15:31:49 +0530

    File updated at 1/20/2026, 3:31:52 PM

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.cpp
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.cpp
@@ -1,49 +1,28 @@
-// Last updated: 1/20/2026, 3:28:59 PM
+// Last updated: 1/20/2026, 3:31:52 PM
 1class Solution {
 2public:
-3    vector<int> searchRange(vector<int>& arr, int target) {
-4        int n = arr.size();
-5        int start = 0;
+3    int findMin(vector<int>& nums) {
+4        int start = 0;
+5        int n = nums.size();
 6        int end = n-1;
-7        int first = -1;
-8        int last = -1;
-9        int mid;
-10        int ar[2];
-11        while(start<=end){
-12            mid = end +(start-end)/2;
-13            if(arr[mid]==target){
-14                first = mid;
-15                end = mid-1;
-16
-17            }
-18            else if(arr[mid]>target){
-19                end = mid -1;
-20            }
-21            else{
-22                start = mid+1;
-23            }
-24        }
-25
-26
-27        start =0;
-28        end = n-1;
-29        while(start<=end){
-30            mid = end +(start-end)/2;
-31            if(arr[mid]==target){
-32                last = mid;
-33                start = mid+1;
-34
-35            }
-36            else if(arr[mid]>target){
-37                end = mid -1;
-38            }
-39            else{
-40                start = mid+1;
-41            }
-42        }
-43        vector<int> a(2);
-44        a[0]=first;
-45        a[1]=last;
-46        return a;
-47    }
-48};
+7        int low = INT_MAX;
+8        if(n==1){
+9            return nums[0];
+10        }
+11        
+12        while(start<=end){
+13          int mid = start-(start-end)/2;
+14          if(nums[start]<=nums[mid]){
+15            low = min(nums[start],low);
+16            start = mid+1;
+17
+18          }  
+19          else{
+20            low = min(low,nums[end]);
+21            end = mid;
+22          }
+23        }
+24        return low;
+25        
+26    }
+27};
