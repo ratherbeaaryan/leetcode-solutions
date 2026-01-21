8975d287265a0c44f52e9df76a53a7b23e6a353e
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2026-01-21 11:23:43 +0530

    File updated at 1/21/2026, 11:23:46 AM

--- 15-3Sum/15-3Sum.cpp
+++ 15-3Sum/15-3Sum.cpp
@@ -1,34 +1,46 @@
-// Last updated: 8/3/2025, 11:57:50 PM
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> res;
-
-        for (int i = 0; i < nums.size() - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1, right = nums.size() - 1;
-            while (left < right) {
-                int total = nums[i] + nums[left] + nums[right];
-                if (total == 0) {
-                    res.push_back({nums[i], nums[left], nums[right]});
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                    left++;
-                    right--;
-                } else if (total < 0) {
-                    left++;
-                } else {
-                    right--;
-                }
-            }
-        }
-
-        return res;
-    }
-};
+// Last updated: 1/21/2026, 11:23:46 AM
+1class Solution {
+2public:
+3    string longestPalindrome(string s) {
+4        if(s.size()<=1){
+5            return s;
+6        }
+7        int start = 0;
+8        int n = s.size();
+9        int maxLength = 1;
+10
+11        for(int i=0;i<s.size();i++){
+12            //odd
+13            int left = i,right = i;
+14            while(left>=0 && right<n && s[left]==s[right]){
+15                int currentLength = right - left +1;
+16                if(currentLength > maxLength){
+17                    maxLength = currentLength;
+18                    start = left;
+19                }
+20                
+21                left--;
+22                right++;
+23                
+24
+25            }
+26            //even
+27            left = i,right = i+1;
+28            while(left>=0 && right<n && s[left]==s[right]){
+29                
+30                int currentLength = right - left +1;
+31                if(currentLength > maxLength){
+32                    maxLength = currentLength;
+33                    start = left;
+34                }
+35                
+36                left--;
+37                right++;
+38
+39            }
+40            
+41
+42        }
+43        return s.substr(start,maxLength);
+44    }
+45};
