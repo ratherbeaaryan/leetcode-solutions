cab4ab02a5f947307ca04862695bfe8ce9957d06
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2026-01-21 12:10:21 +0530

    File updated at 1/21/2026, 12:10:24 PM

--- 15-3Sum/15-3Sum.cpp
+++ 15-3Sum/15-3Sum.cpp
@@ -1,19 +1,20 @@
-// Last updated: 1/21/2026, 12:04:14 PM
+// Last updated: 1/21/2026, 12:10:24 PM
 1class Solution {
 2public:
-3    int majorityElement(vector<int>& nums) {
+3    vector<int> majorityElement(vector<int>& nums) {
 4        unordered_map<int,int> freq;
-5
-6        int n = nums.size();
-7        for(int num:nums){
-8            freq[num]++;
-9            if(freq[num]>n/2){
-10                return num;
-11            }
-12        }
-13        return -1;
+5        set<int> res;
+6        int n= nums.size();
+7
+8        for(int num:nums){
+9            freq[num]++;
+10            if(freq[num]>n/3){
+11                res.insert(num);
+12            }
+13        }
 14
-15             
-16        
-17    }
-18};
+15        vector<int> v(res.begin(),res.end());
+16        return v;
+17        
+18    }
+19};
