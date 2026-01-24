cc8c63e8aafe61e9e320a24626bf9a2029709a59
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2026-01-24 11:49:47 +0530

    File updated at 1/24/2026, 11:49:53 AM

--- 589-N-aryTreePreorderTraversal/589-N-aryTreePreorderTraversal.cpp
+++ 589-N-aryTreePreorderTraversal/589-N-aryTreePreorderTraversal.cpp
@@ -1,45 +1,37 @@
-// Last updated: 1/24/2026, 10:48:04 AM
-1/*
-2// Definition for a Node.
-3class Node {
-4public:
-5    int val;
-6    vector<Node*> children;
-7
-8    Node() {}
-9
-10    Node(int _val) {
-11        val = _val;
-12    }
-13
-14    Node(int _val, vector<Node*> _children) {
-15        val = _val;
-16        children = _children;
-17    }
-18};
-19*/
-20
-21class Solution {
-22public:
-23vector<int> ans;
-24    void preOrder(Node* root){
-25        if(root==NULL){
-26            return;
-27        }
-28        ans.push_back(root->val);
-29        for(Node* child: root->children){
-30            preOrder(child);
+// Last updated: 1/24/2026, 11:49:53 AM
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    void calculatesum(TreeNode* root,int &sum){
+15        if(!root){
+16            return;
+17        }
+18        
+19        if(root->left && !root->left->left && !root->left->right){
+20            sum += root->left->val;
+21        }
+22        calculatesum(root->left,sum);
+23        calculatesum(root->right,sum);
+24        
+25
+26
+27    } 
+28    int sumOfLeftLeaves(TreeNode* root) {
+29        int sum =0;
+30        calculatesum(root,sum);
 31
-32        }
-33    }
-34
-35
-36    vector<int> preorder(Node* root) {
-37        
-38        preOrder(root);
-39        return ans;
-40        
-41
-42        
-43    }
-44};
+32
+33
+34        return sum;
+35    }
+36};
