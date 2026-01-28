73a720e1d13e25bb6d991a3490ec2e843ec3d649
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2026-01-28 12:11:18 +0530

    File updated at 1/28/2026, 12:11:28 PM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
@@ -1,23 +1,29 @@
-// Last updated: 1/28/2026, 10:34:30 AM
-1class Solution {
-2public:
-3    int numberOfSubstrings(string s) {
-4        int n = s.size();
-5        int i = 0;
-6        int count = 0;
-7        int freq[3];
-8
-9        for(int j=0;j<n;j++){
-10            freq[s[j]-'a']++;
-11            while(freq[0] > 0 && freq[1] > 0 && freq[2]>0){
-12                count +=n-j;
-13                
-14                freq[s[i]-'a']--;
-15                i++;
-16            }
-17
-18
-19        }
-20        return count;
+// Last updated: 1/28/2026, 12:11:28 PM
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    void dfs(TreeNode* root,int level,vector<int> &result){
+15        if(!root) return;
+16        if(level == result.size()){
+17            result.push_back(root->val);
+18        }
+19        dfs(root->right,level+1,result);
+20        dfs(root->left,level+1,result);
 21    }
-22};
+22    
+23    vector<int> rightSideView(TreeNode* root) {
+24        vector<int> result;
+25        dfs(root,0,result);
+26        return result;
+27    }
+28};
