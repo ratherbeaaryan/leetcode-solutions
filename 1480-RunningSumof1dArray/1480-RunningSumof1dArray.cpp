1a32b9bd688043bbf0ce5a0949d5adaf7c8077b8
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2026-02-02 01:40:33 +0530

    File updated at 2/2/2026, 1:40:49 AM

--- 1480-RunningSumof1dArray/1480-RunningSumof1dArray.cpp
+++ 1480-RunningSumof1dArray/1480-RunningSumof1dArray.cpp
@@ -1,16 +1,23 @@
-// Last updated: 2/1/2026, 11:48:23 PM
+// Last updated: 2/2/2026, 1:40:49 AM
 1class Solution {
 2public:
-3    vector<int> runningSum(vector<int>& nums) {
+3    int pivotIndex(vector<int>& nums) {
 4        int n = nums.size();
-5        int sum = 0;
-6        for(int i=0;i<n;i++){
-7            sum+= nums[i];
-8            nums[i] = sum;
-9            
-10            
-11        }
-12        return nums;
-13        
-14    }
-15};
+5        int presum = 0;
+6        int sufsum = 0;
+7        for(int x: nums){
+8            presum += x;
+9        }
+10        int leftsum =0;
+11        for(int i=0;i<n;i++){
+12            if(leftsum == presum - leftsum - nums[i]){
+13                return i;
+14            }
+15
+16            leftsum +=nums[i];
+17        }
+18        return -1;
+19
+20        
+21    }
+22};
