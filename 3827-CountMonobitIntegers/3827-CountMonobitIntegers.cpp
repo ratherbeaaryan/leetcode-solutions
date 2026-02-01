f2669436066ef243961ea3f1a2f7f9cec9178ed8
ratherbeaaryan <aaryanakhilgupta@gmail.com>
2026-02-02 01:55:58 +0530

    Create 3827-CountMonobitIntegers

---
+++ 3827-CountMonobitIntegers/3827-CountMonobitIntegers.cpp
@@ -0,0 +1,11 @@
+// Last updated: 2/2/2026, 1:56:14 AM
+1class Solution {
+2public:
+3    int finalElement(vector<int>& nums) {
+4        int n = nums.size();
+5        return max(nums[0],nums.back());
+6        
+7        
+8        
+9    }
+10};
