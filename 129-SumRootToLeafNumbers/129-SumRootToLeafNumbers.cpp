ecb1c7f82910ff6721aa97d5d7f5f95ecbfd4110
Aaryan Gupta <aaryanakhilgupta@gmail.com>
2026-08-04 01:02:17 +0530

    Create 129-SumRootToLeafNumbers

---
+++ 129-SumRootToLeafNumbers/129-SumRootToLeafNumbers.cpp
@@ -0,0 +1,35 @@
+// Last updated: 8/4/2026, 1:02:17 AM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int solve(TreeNode* root,int num){
+
+    if(root==NULL){
+        return 0;
+    }
+    num = num*10+root->val;
+    if(!root->left && !root->right){
+        return num;
+    }
+
+    return solve(root->left,num) + solve(root->right,num);
+
+    
+
+
+}
+    int sumNumbers(TreeNode* root) {
+        return solve(root,0);
+        
+    }
+};
