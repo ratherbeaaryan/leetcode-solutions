ab1819c27e769bc4ec50dc13dc1c38725b75cd74
Aaryan Gupta <aaryanakhilgupta@gmail.com>
2026-08-04 01:03:50 +0530

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.cpp
@@ -0,0 +1,32 @@
+// Last updated: 8/4/2026, 1:03:50 AM
+class Solution {
+public:
+    void permutation(vector<int>&nums,vector<vector<int>>& ans,vector<bool>& visited,vector<int>& temp){
+        if(visited.size()==temp.size()){
+            ans.push_back(temp);
+            return ;
+            
+        }
+        for(int i=0;i<visited.size();i++){
+            if(visited[i]==0){
+                visited[i]=1;
+                temp.push_back(nums[i]);
+                permutation(nums,ans,visited,temp);
+                visited[i]=0;
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector <bool> visited(nums.size(),0);
+        vector <vector<int>> ans;
+        vector <int> temp;
+        
+        permutation(nums,ans,visited,temp);
+        return ans;
+
+
+        
+    }
+
+};
