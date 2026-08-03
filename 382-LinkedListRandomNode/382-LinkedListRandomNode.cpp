d142cbc1a9237957ee0efe125eb3156d44579c6e
Aaryan Gupta <aaryanakhilgupta@gmail.com>
2026-08-04 01:00:04 +0530

    Create 382-LinkedListRandomNode

---
+++ 382-LinkedListRandomNode/382-LinkedListRandomNode.cpp
@@ -0,0 +1,41 @@
+// Last updated: 8/4/2026, 1:00:04 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* Head;
+    Solution(ListNode* head) {
+        Head = head;
+        
+    }
+    
+    int getRandom() {
+        int count = 1;
+        int result = 0;
+        ListNode* temp = Head;
+        
+        while(temp!=NULL){
+            if(rand()%count < 1.0/count){
+                result = temp->val;
+            }
+            count++;
+            temp = temp->next;
+        }
+        return result;
+        
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
