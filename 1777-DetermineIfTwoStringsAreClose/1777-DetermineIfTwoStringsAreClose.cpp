4ff3d29bdea637fe4149e9aec479942cab4d9221
Aaryan Gupta <aaryanakhilgupta@gmail.com>
2026-08-04 00:56:02 +0530

    Create 1777-DetermineIfTwoStringsAreClose

---
+++ 1777-DetermineIfTwoStringsAreClose/1777-DetermineIfTwoStringsAreClose.cpp
@@ -0,0 +1,44 @@
+// Last updated: 8/4/2026, 12:56:02 AM
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        int n = word1.length();
+        int m = word2.length();
+        vector<int> freq1(26);
+        vector<int> freq2(26);
+        if(n!=m){
+            return false;
+        }
+        for(int i=0;i<n;i++){
+            char ch = word1[i];
+            int index = ch-'a';
+            freq1[index]++;
+        }
+        for(int i=0;i<m;i++){
+            char ch = word2[i];
+            int index2 = ch-'a';
+            freq2[index2]++;
+        }
+        for(int i=0;i<26;i++){
+            if(freq1[i]!=0 && freq2[i]!=0){
+                continue;
+            }
+             if(freq1[i]==0 && freq2[i]==0){
+                continue;
+            }
+            
+                return false;
+
+        
+            
+        }
+        sort(begin(freq1),end(freq1));
+        sort(begin(freq2),end(freq2));
+
+        if(freq1==freq2){
+            return true;
+        }
+        return false;
+        
+    }
+};
